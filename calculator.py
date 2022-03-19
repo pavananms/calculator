91d90938d70edf1304435c33c61940642098ff79
pavananms <pavananms@gmail.com>
2022-03-19 16:11:59 +0530

    First test condition done (null string)

---
+++ calculator.py
@@ -0,0 +1,6 @@
+class Calculator:
+    @staticmethod
+    def add(sum_string):
+        if not sum_string:
+             return 0    
+            
---
+++ test_calculator.py
@@ -0,0 +1,4 @@
+from calculator import Calculator 
+
+def test_zero_sum():
+    assert Calculator.add("") == 0
