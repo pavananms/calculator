64cad5c9af555590de0828a66a2484821d046593
pavananms <pavananms@gmail.com>
2022-03-19 21:35:59 +0530

    second test passed (single number)

--- calculator.py
+++ calculator.py
@@ -3,4 +3,7 @@ class Calculator:
     def add(sum_string):
         if not sum_string:
              return 0    
-            
+        if ',' not in sum_string:
+             return int(sum_string)
+
+
--- test_calculator.py
+++ test_calculator.py
@@ -2,3 +2,6 @@ from calculator import Calculator
 
 def test_zero_sum():
     assert Calculator.add("") == 0
+
+def test_single_num():
+    assert Calculator.add("4") == 4
